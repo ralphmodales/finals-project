546ae40516b5e018797e071d8634ab9607ab5979
ralphmodales <148464942+ralphmodales@users.noreply.github.com>
2024-03-12 11:44:23 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,619 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <iomanip>
+#include <unistd.h>
+#include <climits>
+#include "patch.h"
+
+int a_n;
+std::string account_number, first, last, middle, address, birthday, pin, gender, account_str;
+char account;
+float balance, initial_deposit;
+bool isValid, isEighteen;
+
+void clearScreen()
+{
+    system("cls");
+}
+
+void displayAccessDenied()
+{
+    clearScreen();
+    std::cout << "****************************************" << std::endl;
+    std::cout << "*          Access Denied!               *" << std::endl;
+    std::cout << "*      Incorrect PIN. Please try again. *" << std::endl;
+    std::cout << "****************************************" << std::endl;
+    sleep(2);
+}
+
+void displayTransactionSuccess(std::string transactionType, float amount)
+{
+    clearScreen();
+    std::cout << "==================================" << std::endl;
+    std::cout << "|       " << transactionType << " Successful      |" << std::endl;
+    std::cout << "|   Updated Balance: P" << std::fixed << std::setprecision(2) << balance << "   |" << std::endl;
+    std::cout << "==================================" << std::endl;
+    std::cout << "Amount " << transactionType << ": P" << std::fixed << std::setprecision(2) << amount << std::endl;
+    std::cout << "==================================" << std::endl;
+    system("pause");
+}
+
+
+void displayWelcomeScreen()
+{
+    clearScreen();
+    std::cout << "----------------------------------------" << std::endl;
+    std::cout << "|          Welcome to My ATM           |" << std::endl;
+    std::cout << "|          Banking System              |" << std::endl;
+    std::cout << "----------------------------------------" << std::endl;
+}
+
+void displayMainMenu()
+{
+    std::cout << "Main Menu:" << std::endl;
+    std::cout << "1. Open a New Account" << std::endl;
+    std::cout << "2. Balance Inquiry" << std::endl;
+    std::cout << "3. Deposit" << std::endl;
+    std::cout << "4. Withdraw" << std::endl;
+    std::cout << "5. Account Information" << std::endl;
+    std::cout << "6. Delete Account" << std::endl;
+    std::cout << "7. Change PIN" << std::endl;
+    std::cout << "8. Exit" << std::endl;
+    std::cout << "----------------------------------------" << std::endl;
+    std::cout << "Enter your choice: ";
+}
+
+void displayBalance()
+{
+    clearScreen();
+    std::cout << "----------------------------------------" << std::endl;
+    std::cout << "|          Balance Inquiry             |" << std::endl;
+    std::cout << "|          Current Balance: P" << std::fixed << std::setprecision(2) << balance << "   |" << std::endl;
+    std::cout << "----------------------------------------" << std::endl;
+    system("pause");
+}
+
+void displayNewAccountMenu()
+{
+    clearScreen();
+    std::cout << "----------------------------------------" << std::endl;
+    std::cout << "|        Open a New Account            |" << std::endl;
+    std::cout << "----------------------------------------" << std::endl;
+}
+
+void displayPINGenerated()
+{
+    clearScreen();
+    std::cout << "----------------------------------------" << std::endl;
+    std::cout << "|           PIN Generated              |" << std::endl;
+    std::cout << "|           Your PIN is: " << pin << "          |" << std::endl;
+    std::cout << "----------------------------------------" << std::endl;
+    system("pause");
+}
+
+bool checkPin()
+{
+    clearScreen();
+    std::string guess;
+
+    std::cout << "----------------------------------------" << std::endl;
+    std::cout << "|               Enter PIN               |" << std::endl;
+    std::cout << "----------------------------------------" << std::endl;
+
+    std::cout << "Enter your PIN: ";
+    std::cin >> guess;
+
+    if (strcmp(guess.c_str(), pin.c_str()) == 0)
+    {
+        std::cout << "PIN Verified. Access Granted." << std::endl;
+        sleep(2);
+        return true;
+    }
+    else
+    {
+        displayAccessDenied();
+        return false;
+    }
+}
+
+void changePin()
+{
+    int new_pin;
+    if(checkPin())
+    {
+        while(true)
+        {
+            std::cout << "New pin: ";
+            std::cin >> new_pin;
+            if(std::cin.fail() || new_pin >= 1000 || new_pin < 0)
+            {
+                std::cin.clear();
+                std::cin.ignore(INT_MAX, '\n');
+                std::cout << "Invalid Input. Enter a valid PIN. " << std::endl;
+            }
+            else
+                break;
+        }
+        pin = patch::to_string(new_pin);
+        pin = pin.size() < 4 ? std::string(4 - pin.size(), '0') + pin : pin;
+        displayPINGenerated();
+    }
+    else
+    {
+        displayAccessDenied();
+        system("pause");
+        return;
+    }
+}
+
+void deleteAccount()
+{
+    clearScreen();
+    char choice;
+    if(checkPin())
+    {
+        while(true)
+        {
+            clearScreen();
+            std::cout << "\n========================================" << std::endl;
+            std::cout << "|         Delete Your Account          |" << std::endl;
+            std::cout << "========================================" << std::endl;
+            std::cout << "Are you sure you want to delete your account? (Enter 'y' if yes and 'n' if no)\nInput: ";
+            std::cin >> choice;
+            if(choice == 'y' || choice == 'Y')
+            {
+                account_number.clear();
+                first.clear();
+                last.clear();
+                middle.clear();
+                address.clear();
+                birthday.clear();
+                pin.clear();
+                gender.clear();
+                account_str.clear();
+                account = '\0';
+                balance = 0;
+                initial_deposit = 0;
+                isValid = false;
+                isEighteen = false;
+                std::cout << "\n========================================" << std::endl;
+                std::cout << "|       Account Deleted Successfully    |" << std::endl;
+                std::cout << "========================================\n" << std::endl;
+                system("pause");
+                break;
+            }
+            else if(choice == 'n' || choice == 'N')
+            {
+                std::cout << "\n========================================" << std::endl;
+                std::cout << "|     Account Deletion Canceled        |" << std::endl;
+                std::cout << "========================================\n" << std::endl;
+                system("pause");
+                break;
+            }
+            else
+            {
+
+                std::cout << "\n========================================" << std::endl;
+                std::cout << "|        Invalid Input! Try Again       |" << std::endl;
+                std::cout << "========================================\n" << std::endl;
+                system("pause");
+            }
+        }
+    }
+    else
+    {
+        displayAccessDenied();
+        return;
+    }
+}
+
+void withdraw()
+{
+    if(checkPin())
+    {
+        bool dec = true;
+        float withdraw_money;
+        while(dec)
+        {
+            clearScreen();
+            std::cout << "==================================" << std::endl;
+            std::cout << "            WITHDRAW               " << std::endl;
+            std::cout << "==================================" << std::endl;
+            std::cout << "Withdraw: P";
+            std::cin >> withdraw_money;
+            if(withdraw_money <= balance && withdraw_money >= 300 && account == 's' || account == 'S')
+            {
+                balance -= withdraw_money;
+                dec = false;
+                displayTransactionSuccess("Withdraw", withdraw_money);
+            }
+            else if(withdraw_money >= 500 && withdraw_money <= balance && account == 'c' || account == 'C')
+            {
+                balance -= withdraw_money;
+                dec = false;
+                displayTransactionSuccess("Withdraw", withdraw_money);
+            }
+            else
+            {
+                std::cout << "Invalid input. ";
+                if(withdraw_money > balance)
+                    std::cout << "Insufficient Balance. ";
+                std::cout << "Enter x to cancel the transaction. " << std::endl;
+            }
+        }
+    }
+    else
+    {
+        displayAccessDenied();
+        system("pause");
+    }
+}
+
+void deposit()
+{
+
+    if(checkPin())
+    {
+        bool dec = true;
+        float deposit_money;
+        while(dec)
+        {
+            clearScreen();
+            std::cout << "==================================" << std::endl;
+            std::cout << "            DEPOSIT               " << std::endl;
+            std::cout << "==================================" << std::endl;
+            std::cout << "Deposit: P";
+            std::cin >> deposit_money;
+            if(deposit_money >= 300 && account == 's' || account == 'S')
+            {
+                balance += deposit_money;
+                dec = false;
+                displayTransactionSuccess("Deposit", deposit_money);
+            }
+            else if(deposit_money >= 500 && account == 'c' || account == 'C')
+            {
+                balance += deposit_money;
+                dec = false;
+                displayTransactionSuccess("Deposit", deposit_money);
+            }
+            else
+            {
+                std::cout << "\nInvalid Input. Minimum deposit amount for ";
+                if(account == 's' || account == 'S')
+                    std::cout << "savings account is P300. ";
+                else if(account == 'c' || account == 'C')
+                    std::cout << "current account is P500. ";
+
+                std::cout << "Enter x to cancel the transaction. " << std::endl;
+
+            }
+
+        }
+    }
+    else
+    {
+        displayAccessDenied();
+        system("pause");
+    }
+}
+
+void balance_check()
+{
+    clearScreen();
+    if (checkPin())
+    {
+        displayBalance();
+    }
+    else
+    {
+        displayAccessDenied();
+        system("pause");
+    }
+}
+
+void viewAccountInformation()
+{
+    clearScreen();
+    if (checkPin())
+    {
+        clearScreen();
+        std::cout << "----------------------------------------" << std::endl;
+        std::cout << "|        Account Information           |" << std::endl;
+        std::cout << "----------------------------------------" << std::endl;
+        std::cout << "| Account Number: " << account_number << std::endl;
+        std::cout << "| Name: " << first << " " << middle << " " << last << std::endl;
+        std::cout << "| Address: " << address << std::endl;
+        std::cout << "| Birthdate: " << birthday << std::endl;
+        std::cout << "| Gender: " << gender << std::endl;
+        std::cout << "| Account Type: " << account_str << std::endl;
+        std::cout << "| Initial Deposit: P" << std::fixed << std::setprecision(2) << initial_deposit << std::endl;
+        std::cout << "| Current Balance: P" << std::fixed << std::setprecision(2) << balance << std::endl;
+        std::cout << "----------------------------------------" << std::endl;
+        system("pause");
+    }
+    else
+    {
+        displayAccessDenied();
+        system("pause");
+    }
+}
+
+void pin_generator()
+{
+    a_n = rand() % 10000;
+    pin = patch::to_string(a_n);
+    pin = pin.size() < 4 ? std::string(4 - pin.size(), '0') + pin : pin;
+
+    sleep(3);
+    displayPINGenerated();
+}
+
+void account_type()
+{
+    bool dec = true;
+    char choice;
+    double deposit_money;
+    while (dec)
+    {
+        std::cout << "Choose an account type:" << std::endl;
+        std::cout << "S - Savings Account (Minimum deposit: P5000)" << std::endl;
+        std::cout << "C - Current Account (Minimum deposit: P10000)" << std::endl;
+        std::cout << "Enter your choice: ";
+        std::cin >> choice;
+
+        if (choice == 'S' || choice == 's')
+        {
+            do
+            {
+                std::cout << "Enter the initial deposit amount (P5000 or more): P";
+                std::cin >> deposit_money;
+
+                if(deposit_money < 5000)
+                    std::cout << "Initial deposit is not sufficient. Please try again." << std::endl;
+            } while(deposit_money < 5000);
+
+            balance = deposit_money;
+            initial_deposit = deposit_money;
+            account = choice;
+            dec = false;
+        }
+        else if (choice == 'C' || choice == 'c')
+        {
+            do
+            {
+                std::cout << "Enter the initial deposit amount (P10000 or more): P";
+                std::cin >> deposit_money;
+
+                if(deposit_money < 10000)
+                    std::cout << "Initial deposit is not sufficient. Please try again." << std::endl;
+            } while(deposit_money < 10000);
+
+            balance = deposit_money;
+            initial_deposit = deposit_money;
+            account = choice;
+            dec = false;
+        }
+        else
+        {
+            std::cout << "Invalid input. Please choose 'S' for Savings or 'C' for Current Account." << std::endl;
+        }
+    }
+    std::cout << "Account type selected successfully!" << std::endl;
+    system("pause");
+}
+
+void gender_checker()
+{
+    bool isAllowed = true;
+    char input;
+    while (isAllowed)
+    {
+        std::cout << "Gender (M/F): ";
+        std::cin >> input;
+        if (input == 'M' || input == 'm')
+        {
+            gender = "Male";
+            isAllowed = false;
+        }
+        else if (input == 'F' || input == 'f')
+        {
+            gender = "Female";
+            isAllowed = false;
+        }
+        else
+        {
+            std::cin.clear();
+            std::cin.ignore(INT_MAX, '\n');
+            std::cout << "Enter a valid gender." << std::endl;
+        }
+    }
+}
+
+void birthdate()
+{
+    bool dec = true;
+    while (dec)
+    {
+        std::cout << "Birthdate (MM/DD/YYYY): ";
+        std::cin >> birthday;
+        if (birthday.size() == 10)
+        {
+            std::istringstream bd(birthday);
+            int month, day, year;
+            char slash;
+
+            if (bd >> month >> slash >> day >> slash >> year && slash == '/')
+            {
+                if (month >= 1 && month <= 12 && day >= 1 && day <= 31 && year >= 1900 && (2024 - year) >= 18)
+                {
+                    isValid = true;
+                    isEighteen = true;
+                    dec = false;
+                }
+                else if (2024 - year < 18)
+                {
+                    clearScreen();
+                    std::cout << "\n========================================" << std::endl;
+                    std::cout << "|    Sorry, You Are Not Eligible.       |" << std::endl;
+                    std::cout << "|   Your Age is Not Allowed to Open     |" << std::endl;
+                    std::cout << "|           an Account.                 |" << std::endl;
+                    std::cout << "========================================\n" << std::endl;
+                    system("pause");
+                    return;
+                }
+            }
+            else
+            {
+                std::cout << "Invalid Input. Enter a valid birthdate." << std::endl;
+            }
+        }
+        else
+        {
+            std::cout << "Invalid Input. Enter a valid birthdate." << std::endl;
+        }
+    }
+}
+
+void personal_info()
+{
+    clearScreen();
+    std::cin.ignore();
+
+    std::cout << "----------------------------------------" << std::endl;
+    std::cout << "|          Personal Information         |" << std::endl;
+    std::cout << "----------------------------------------" << std::endl;
+
+    std::cout << "First name: ";
+    getline(std::cin, first);
+
+    std::cout << "Last name: ";
+    getline(std::cin, last);
+
+    std::cout << "Middle name: ";
+    getline(std::cin, middle);
+
+    std::cout << "Address: ";
+    getline(std::cin, address);
+
+    birthdate();
+    if(isEighteen)
+        gender_checker();
+}
+
+void accnum_generator()
+{
+    clearScreen();
+    a_n = rand() % 10000;
+    account_number = patch::to_string(a_n);
+    account_number = account_number.size() < 4 ? std::string(4 - account_number.size(), '0') + account_number : account_number;
+}
+
+void new_account()
+{
+    clearScreen();
+    accnum_generator();
+    personal_info();
+    if (isValid)
+    {
+        account_type();
+        pin_generator();
+    }
+    else
+    {
+        return;
+    }
+    clearScreen();
+}
+
+int main(void)
+{
+    int choice;
+    bool dec = true;
+    srand(time(0));
+
+    while (dec)
+    {
+        displayWelcomeScreen();
+        displayMainMenu();
+        std::cin >> choice;
+
+        switch (choice)
+        {
+            case 1:
+                new_account();
+                break;
+            case 2:
+                if(isValid)
+                    balance_check();
+                else
+                {
+                    std::cout << "Invalid input. Please open a new account first." << std::endl;
+                    system("pause");
+                }
+                break;
+            case 3:
+                if(isValid)
+                    deposit();
+                else
+                {
+                    std::cout << "Invalid input. Please open a new account first." << std::endl;
+                    system("pause");
+                }
+                break;
+            case 4:
+                if (isValid)
+                    withdraw();
+                else
+                {
+                    std::cout << "Invalid input. Please open a new account first." << std::endl;
+                    system("pause");
+                }
+                break;
+            case 5:
+                if(isValid)
+                    viewAccountInformation();
+                else
+                {
+                    std::cout << "Invalid input. Please open a new account first." << std::endl;
+                    system("pause");
+                }
+                break;
+            case 6:
+                if(isValid)
+                    deleteAccount();
+                else
+                {
+                    std::cout << "Invalid input. Please open a new account first." << std::endl;
+                    system("pause");
+                }
+                break;
+            case 7:
+                if(isValid)
+                    changePin();
+                else
+                {
+                    std::cout << "Invalid input. Please open a new account first." << std::endl;
+                    system("pause");
+                }
+                break;
+            case 8:
+                dec = false;
+                clearScreen();
+                std::cout << "\n========================================" << std::endl;
+                std::cout << "|        Thank you for using ATM.       |" << std::endl;
+                std::cout << "|              Goodbye!                 |" << std::endl;
+                std::cout << "========================================\n" << std::endl;
+                sleep(2);
+                break;
+            default:
+                std::cout << "Invalid input. Please try again." << std::endl;
+                system("pause");
+        }
+        clearScreen();
+    }
+
+    return 0;
+}
+
